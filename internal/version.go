ca6732470cbe19643d61ca06529f569fb1979302
seanseannery <seansemail@example.com>
2026-03-05 16:11:02 -0800

    ci: seperating pr workflows for different situations and updating version bump default logic bug

--- internal/version.go
+++ internal/version.go
@@ -4,4 +4,8 @@ package internal
 // Override at build time with:
 //
 //	go build -ldflags="-X sean_seannery/opsfile/internal.Version=1.2.3" ./cmd/ops/
+<<<<<<< HEAD
 var Version = "0.6.0"
+=======
+var Version = "0.5.3"
+>>>>>>> 1912896 (ci: seperating pr workflows for different situations and updating version bump default logic bug)
